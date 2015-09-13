c28f9f57abfaea137a74013fa62b364fe080f92b
Momo <momothereal.mc@gmail.com>
2015-09-12 23:47:51 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CS:GO Forge - Paid Raffles</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+	<div class="header">
+		<img src="img/logoforge.png" class="header-logo shadow">
+		<a href="#"><div class="header-button selected">Paid Raffles</div></a><a href="#	"><div class="header-button">Free Raffles</div></a>
+	</div>
+
+	<div class="top-notice">Current Raffle</div>
+	<div class="item-title">Gut Knife | Doppler (Factory New)</div>
+	<img src="https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJf1ObcTjxP09i5hJCHkuXLI7PQhW4A18l4jeHVu9703Azs-hA_MTuncNWWIVU-aF7Z_1a7k-bo0cW_v8_OyXVqvyAqsy3D30vgdDGy9vw/512fx512f" class="item-showcase shadow">
+	
+	<div class="footer">&copy; CS:GO Forge &nbsp;&mdash;&nbsp; <a href="mailto:official@csgoforge.com">Contact us via e-mail</a> &nbsp;&mdash;&nbsp; <strong><a href="http://steamcommunity.com/">Powered by Steam</a></strong></div>
+
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
